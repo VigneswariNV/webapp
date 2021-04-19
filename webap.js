622cb5f8a5880ddf0fe5c92a5f89dabba9d9a001
Vigneswari NV <nvvigneswari24@gmail.com>
2021-04-19 17:50:49 +0530

    dropdown menu

--- webap.js
+++ webap.js
@@ -10,3 +10,41 @@ toggle.addEventListener('input', e => {
         body.classList.remove('dark-theme');
     }
 });
+
+
+var slideIndex = [1,1];
+var slideId = ["sub-element", "element"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "inline-block";  
+}
+
+
+let dropdownBtn = document.querySelector('.drop-btn');
+let menuContent = document.querySelector('.dropdown-content');
+let upIcon = document.querySelector('.up');
+let downIcon = document.querySelector('.down');
+dropdownBtn.addEventListener('click',()=>{
+   if(menuContent.style.display===""){
+      menuContent.style.display="block";
+      downIcon.style.display="";
+      upIcon.style.display="inline-block";
+   } else {
+      menuContent.style.display="";
+      downIcon.style.display="inline-block";
+      upIcon.style.display="";
+   }
+})
