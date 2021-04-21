8cdfcfc44a5c13307a1767c8d6f0cfeed25c5dee
Vigneswari NV <nvvigneswari24@gmail.com>
2021-04-21 18:40:07 +0530

    scroll bar changes

--- webap.js
+++ webap.js
@@ -12,25 +12,25 @@ toggle.addEventListener('input', e => {
 });
 
 
-var slideIndex = [1,1];
-var slideId = ["sub-element", "element"]
-showSlides(1, 0);
-showSlides(1, 1);
+//var slideIndex = [1,1];
+//var slideId = ["sub-element", "element"]
+//showSlides(1, 0);
+//showSlides(1, 1);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
+//function plusSlides(n, no) {
+  //showSlides(slideIndex[no] += n, no);
+//}
 
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "inline-block";  
-}
+//function showSlides(n, no) {
+  //var i;
+  //var x = document.getElementsByClassName(slideId[no]);
+  //if (n > x.length) {slideIndex[no] = 1}    
+  //if (n < 1) {slideIndex[no] = x.length}
+  //for (i = 0; i < x.length; i++) {
+    //x[i].style.display = "none";  
+  //}
+  //x[slideIndex[no]-1].style.display = "block";  
+//}
 
 
 let dropdownBtn = document.querySelector('.drop-btn');
@@ -40,11 +40,101 @@ let downIcon = document.querySelector('.down');
 dropdownBtn.addEventListener('click',()=>{
    if(menuContent.style.display===""){
       menuContent.style.display="block";
-      downIcon.style.display="";
+      downIcon.style.display="none";
       upIcon.style.display="inline-block";
    } else {
       menuContent.style.display="";
       downIcon.style.display="inline-block";
       upIcon.style.display="";
    }
+})
+
+
+
+const carousel = document.querySelector("[data-target='slide']");
+const card = carousel.querySelector(".element");
+const leftButton = document.querySelector("[data-action='slideLeft']");
+const rightButton = document.querySelector("[data-action='slideRight']");
+
+const slideWidth=carousel.offsetWidth;
+const carouselWidth = card.offsetWidth;
+const cardStyle = card.currentStyle || window.getComputedStyle(card)
+const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+console.log(carouselWidth);
+console.log( slideWidth);
+console.log(cardMarginRight);
+
+const cardCount = carousel.querySelectorAll(".element").length;
+// console.log(cardCount);
+
+
+let offset = 0;
+const maxX = -((cardCount / 4) * carouselWidth +
+    (cardMarginRight * (cardCount / 4)) -
+    carouselWidth - cardMarginRight );
+
+// console.log(maxX);
+const minN = -((carouselWidth + cardMarginRight) * (cardCount / 4) - maxX);
+// console.log(minN);
+
+
+leftButton.addEventListener("click", function() {
+    if (offset !== 0) {
+        offset += carouselWidth + cardMarginRight;
+        carousel.style.transform = `translateX(${offset}px)`;
+    }
+})
+
+rightButton.addEventListener("click", function() {
+    if (offset > minN) {
+        offset -= carouselWidth + cardMarginRight;
+        carousel.style.transform = `translateX(${offset}px)`;
+        // console.log(offset);
+
+    }
+})
+
+
+
+const subcarousel = document.querySelector("[data-target='sub-slide']");
+const subcard = subcarousel.querySelector(".sub-element");
+const subleftButton = document.querySelector("[data-action='subslideLeft']");
+const subrightButton = document.querySelector("[data-action='subslideRight']");
+
+const subslideWidth=subcarousel.offsetWidth;
+const subcarouselWidth = subcard.offsetWidth;
+const subcardStyle = subcard.currentStyle || window.getComputedStyle(subcard)
+const subcardMarginRight = Number(subcardStyle.marginRight.match(/\d+/g)[0]);
+console.log(subcarouselWidth);
+console.log( subslideWidth);
+console.log(subcardMarginRight);
+
+const subcardCount = subcarousel.querySelectorAll(".sub-element").length;
+// console.log(cardCount);
+
+
+let suboffset = 0;
+const submaxX = -((subcardCount / 4) * subcarouselWidth +
+    (subcardMarginRight * (subcardCount / 4)) -
+    subcarouselWidth - subcardMarginRight );
+
+// console.log(maxX);
+const subminN = -((subcarouselWidth + subcardMarginRight) * (subcardCount / 4) - submaxX);
+// console.log(minN);
+
+
+subleftButton.addEventListener("click", function() {
+    if (suboffset !== 0) {
+        suboffset += subcarouselWidth + subcardMarginRight;
+        subcarousel.style.transform = `translateX(${suboffset}px)`;
+    }
+})
+
+subrightButton.addEventListener("click", function() {
+    if (suboffset > subminN) {
+        suboffset -= subcarouselWidth + subcardMarginRight;
+        subcarousel.style.transform = `translateX(${suboffset}px)`;
+        // console.log(offset);
+
+    }
 })
